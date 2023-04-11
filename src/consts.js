6573a233e19ce967688e803a28e40a0526e4915c
Yuxin-H <scarecrow.hyx@icloud.com>
2023-04-11 17:38:42 +0800

    update

--- src/consts.js
+++ src/consts.js
@@ -1,11 +1,11 @@
-export const SITE_TITLE = `Air Blog`;
+export const SITE_TITLE = `Nix Life`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_EMAIL = 'nix.hou@icloud.com'
+export const SITE_NAME = 'nixhou.xlog.app';
+export const SITE_URL = "https://nixhou.xlog.app/";
 export const SITE_LANG = "zh-CN";
 
 // Author, used only when author is not specified on the page, 
 // has lower priority than the author specified in the md file.
 // 作者信息，仅用在文章没有指定作者的时候，优先显示在 md 文件中指定的作者。
-export const SITE_AUTHOR = "Austin";
+export const SITE_AUTHOR = "Nix";
